c8094b47a3a319f7955e9e4b93020b7da65c5269
kk1Dev <kpkundan243@gmail.com>
2023-04-22 08:47:56 +0530

    deleted:    .env 	deleted:    backend/configs/auth.config.js 	deleted:    backend/configs/db.config.js 	deleted:    backend/configs/email.config.js 	deleted:    backend/configs/server.config.js 	deleted:    backend/notifier/emailService.js

--- backend/configs/auth.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    secret : "Kundan-1995-243"
-}
--- backend/middlewares/index.js
+++
@@ -1,8 +0,0 @@
-const verifyMovieReqBody = require("./verifyMovieReqBody");
-const verifyTheatreReqBody = require("./verifyTheatreReqBody")
-const verifyUserReqBody = require('./verifyUserReqBody')
-
-
-
-
-module.exports = { verifyMovieReqBody , verifyTheatreReqBody ,verifyUserReqBody};
